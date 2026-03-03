a7e2cc330fb2297c17e9c2abf8638815fe1d68e3
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-03-03 16:31:26 +0100

    Implementazione Progetto
    Conclusione delle classi Buca e Talpa, aiuto tramite intelligenza artificiale

--- ProgettoTalpa/src/progettotalpa/Buca.java
+++ ProgettoTalpa/src/progettotalpa/Buca.java
@@ -4,10 +4,31 @@
  */
 package progettotalpa;
 
-/**
- *
- * @author montedori.riccardo
- */
-public class Buca {
+import javax.swing.*;
+import java.awt.*;
+
+public class Buca extends JButton {
+    private boolean occupata = false;
+
+    public Buca() {
+        this.setBackground(Color.GRAY); // Colore della buca vuota
+        this.setText("Vuota"); // Temporaneo per test
+    }
+
+    public synchronized void mostraTalpa() {
+        this.occupata = true;
+        setIcon(new ImageIcon("immagineTalpa.webp"));
+        this.setBackground(Color.RED); 
+        this.setText("TALPA!"); 
+    }
+
+    public synchronized void nascondiTalpa() {
+        this.occupata = false;
+        this.setBackground(Color.GRAY);
+        this.setText("Vuota");
+    }
     
+    public boolean isOccupata() {
+        return occupata;
+    }
 }
--- ProgettoTalpa/src/progettotalpa/Talpa.java
+++ ProgettoTalpa/src/progettotalpa/Talpa.java
@@ -9,10 +9,34 @@ package progettotalpa;
  * @author montedori.riccardo
  */
 public class Talpa extends Thread {
+
     private Buca bucaAssociata;
     private boolean inEsecuzione = true;
 
     public Talpa(Buca buca) {
         this.bucaAssociata = buca;
+    }
+
+    @Override
+    public void run() {
+        while (inEsecuzione) {
+            try {
+
+                Thread.sleep((long) (Math.random() * 2000) + 1000);
+
+                bucaAssociata.mostraTalpa();
+
+                Thread.sleep((long) (Math.random() * 1000) + 500);
+
+                bucaAssociata.nascondiTalpa();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void fermaTalpa() {
+        this.inEsecuzione = false;
     }
 }
