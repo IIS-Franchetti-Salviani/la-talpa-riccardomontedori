a8cc0c1a0b9be8615bf0e6f5109c2ee9fbdf7647
riccardomontedori <montedori.riccardo@franchettisalviani.net>
2026-03-03 17:11:41 +0100

    Completamento Interfaccia

--- ProgettoTalpa/src/progettotalpa/InterfacciaTalpa.java
+++ ProgettoTalpa/src/progettotalpa/InterfacciaTalpa.java
@@ -29,6 +29,18 @@ public class InterfacciaTalpa extends javax.swing.JFrame {
         setSize(500, 500);
         getContentPane().setLayout(new GridLayout(2, 2, 10, 10));
     }
+    
+    private void personalizzaInizializzazione() {
+        for (int i = 0; i < 4; i++) {
+            buche[i] = new Buca();
+            getContentPane().add(buche[i]);
+            talpe[i] = new Talpa(buche[i]);
+            talpe[i].start();
+        }
+
+        revalidate();
+        repaint();
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -59,24 +71,7 @@ public class InterfacciaTalpa extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new InterfacciaTalpa().setVisible(true));
     }
 
